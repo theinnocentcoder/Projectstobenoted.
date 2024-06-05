0c8937a4d59950dc3ccfc918ab924a09cce59c29
theinnocentcoder <nayaksantilata21@gmail.com>
2024-06-05 19:58:14 +0530

    Add files via upload
    Hey guys ! Myself  Sayan Nayak. I am an UG commencing my BCA(Machine Learning) at GD Goenka University, Sohna (2024 batch). I have been a computer enthusiast since a very young age. I am well acquainted with languages like Java and on my go to conquer Python as well. This is my first Python program about a calculator. I hope this goes well and I hope to upload more codes as well. Thank you >>>>

---
+++ calculatorusinguserinput.py
@@ -0,0 +1,17 @@
+print("*************CALCULATOR****************")
+print("Enter the first number:")
+a = input()
+print("Enter the second number:")
+b = input()
+print (" What operaton do you want to perform nigg@??")  #enter the signs
+c = input()
+if c== "+":
+    print ("Addition is:", int(a)+int(b))
+if c== "-":
+    print ("Subtraction is:", int(a)-int(b))
+if c== "*":
+     print ("Multiplication is:", int(a)*int(b))
+if c == "/":
+ print ("Division is:", int(a)/int(b))
+ if c == "%":
+  print ("Modulus is:", int(a)%int(b)) # sign to find the remainder
