b859a74feddc7fcb0f3437a3a524f87800381ee2
SayanX777 <nayaksantilata21@gmail.com>
2025-02-13 22:37:28 +0530

    Add files via upload

---
+++ positivenumbersinlist.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "05614559-929c-45fc-8a6f-9fe0d1b24880",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "12\n",
+      "25\n",
+      "55\n",
+      "60\n",
+      "47\n"
+     ]
+    }
+   ],
+   "source": [
+    "#program to print all positive numbers in the list\n",
+    "list=[12,25,-4,55,60,47,-21]\n",
+    "for x in list:\n",
+    "    if x>=0:\n",
+    "     print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7859d626-19d6-402e-a83d-54cb42542fed",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
